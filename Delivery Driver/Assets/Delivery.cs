1b41da8e8d80e0bbded10990f3cfd16affac85f3
StjepanMat <stjepan.mateljan@gmail.com>
2023-06-28 17:58:35 +0200

    after 23. (If Statements & Tags)

---
+++ Delivery Driver/Assets/Delivery.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Delivery : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("Bump!");
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.tag == "Package")
+        {
+            Debug.Log("Package picked up!");     
+      
+        }
+        else if (other.tag == "Customer")
+        {
+            Debug.Log("Package delivered!");
+        }
+        
+    }
+
+}
