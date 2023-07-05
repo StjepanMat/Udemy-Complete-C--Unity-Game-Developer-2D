ff40d21d3bd5aa7d4c027f275f1ee3df3dd9e47d
Stjepan Mateljan <stjepan.mateljan@gmail.com>
2023-07-05 10:07:22 +0200

    after 25. How To Destroy Objects

--- Delivery Driver/Assets/Delivery.cs
+++ Delivery Driver/Assets/Delivery.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    [SerializeField] float destroyDelay = 1;
     bool hasPackage; //default:false
     void OnCollisionEnter2D(Collision2D other)
     {
@@ -12,10 +13,11 @@ public class Delivery : MonoBehaviour
     void OnTriggerEnter2D(Collider2D other)
     {
 
-        if (other.tag == "Package" && hasPackage!=true)
+        if (other.tag == "Package" && !hasPackage)
         {
             Debug.Log("Package picked up!");
             hasPackage = true;
+            Destroy(other.gameObject, destroyDelay);
       
         }
         else if (other.tag == "Customer" && hasPackage)
