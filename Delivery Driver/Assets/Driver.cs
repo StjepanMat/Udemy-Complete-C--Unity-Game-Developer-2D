9473b0920d1e1a15d51047aa5d5ece3629100523
StjepanMat <stjepan.mateljan@gmail.com>
2023-07-12 12:11:35 +0200

    after 27. Boosts & Bumps

--- Delivery Driver/Assets/Driver.cs
+++ Delivery Driver/Assets/Driver.cs
@@ -6,12 +6,16 @@ public class Driver : MonoBehaviour
 {
     [SerializeField] float steerSpeed = 300f;
     [SerializeField] float driveSpeed = 20f;
+    [SerializeField] float slowSpeed = 10f;
+    [SerializeField] float boostSpeed = 40f;
+
+
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -23,5 +27,21 @@ public class Driver : MonoBehaviour
         transform.Rotate(0, 0, -steerAmount);
         transform.Translate(0, driveAmount, 0);
     }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Boost")
+        {
+            //Debug.Log("Boost works!");
+            driveSpeed = boostSpeed;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        driveSpeed = slowSpeed;     
+    }
+    
+
+       
 }
- 
